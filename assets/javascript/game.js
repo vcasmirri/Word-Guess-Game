a7baf37a9e9a6f9809a9a01fd0d52bd3a0be20ba
beardykraken <vcasmirri@gmail.com>
2020-01-14 16:01:40 -0500

    Tweaked currentWord function and added comment to guide next steps.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,12 +6,18 @@ var guessedLetters = [];
 
 if (winCounter === 0) {
     wordChallenge = word_1;
+    for (var i = 0; i < wordChallenge.length; i++) {
+        currentWord.innerHTML += "_";
+    }
     console.log("The current word is " + wordChallenge + ".");
 }
+
+document.getElementById("winCounter").innerHTML = winCounter;
 
 document.onkeypress = function(evt) {
     evt = evt || window.event;
     var userGuess = event.key;
+    currentWord.innerHTML = "";
     if (guessedLetters.includes(userGuess) === false) {
         if (userGuess) {
             guessedLetters.push(String(userGuess));
@@ -28,6 +34,11 @@ document.onkeypress = function(evt) {
             }
     }
 }
+
+// next, see why currentWord disappears when inputting key not already present in guessedLetters array. likely needs an else or else if statement
+
+
+
 
 // for (var i = 0; i < wordChallenge.length; i++) {
 //     if (guessedLetters.includes(wordChallenge[i])) {
